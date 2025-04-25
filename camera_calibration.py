143ab798177f426de38fbacba91242644e129b95
Joao Silva <jpls10190@gmail.com>
2025-04-25 10:53:36 +0100

    f

--- camera_calibration.py
+++ camera_calibration.py
@@ -18,29 +18,30 @@ img_ptsR = []
 obj_pts = []
  
 for i in tqdm(range(1,12)):
-    imgL = cv2.imread(pathL+"img%d.png"%i)
-    imgR = cv2.imread(pathR+"img%d.png"%i)
-    imgL_gray = cv2.imread(pathL+"img%d.png"%i,0)
-    imgR_gray = cv2.imread(pathR+"img%d.png"%i,0)
-
-    outputL = imgL.copy()
-    outputR = imgR.copy()
-
-    retR, cornersR =  cv2.findChessboardCorners(outputR,(9,6),None)
-    retL, cornersL = cv2.findChessboardCorners(outputL,(9,6),None)
-
-    if retR and retL:
-        obj_pts.append(objp)
-        cv2.cornerSubPix(imgR_gray,cornersR,(11,11),(-1,-1),criteria)
-        cv2.cornerSubPix(imgL_gray,cornersL,(11,11),(-1,-1),criteria)
-        cv2.drawChessboardCorners(outputR,(9,6),cornersR,retR)
-        cv2.drawChessboardCorners(outputL,(9,6),cornersL,retL)
-        cv2.imshow('cornersR',outputR)
-        cv2.imshow('cornersL',outputL)
-        cv2.waitKey(0)
-
-        img_ptsL.append(cornersL)
-        img_ptsR.append(cornersR)
+ imgL = cv2.imread(pathL+"img%d.png"%i)
+ imgR = cv2.imread(pathR+"img%d.png"%i)
+ imgL_gray = cv2.imread(pathL+"img%d.png"%i,0)
+ imgR_gray = cv2.imread(pathR+"img%d.png"%i,0)
+ 
+ outputL = imgL.copy()
+ outputR = imgR.copy()
+ 
+ retR, cornersR =  cv2.findChessboardCorners(outputR,(9,6),None)
+ retL, cornersL = cv2.findChessboardCorners(outputL,(9,6),None)
+ 
+ if retR and retL:
+  obj_pts.append(objp)
+  cv2.cornerSubPix(imgR_gray,cornersR,(11,11),(-1,-1),criteria)
+  cv2.cornerSubPix(imgL_gray,cornersL,(11,11),(-1,-1),criteria)
+  cv2.drawChessboardCorners(outputR,(9,6),cornersR,retR)
+  cv2.drawChessboardCorners(outputL,(9,6),cornersL,retL)
+  cv2.imshow('cornersR',outputR)
+  cv2.imshow('cornersL',outputL)
+  cv2.waitKey(0)
+ 
+  img_ptsL.append(cornersL)
+  img_ptsR.append(cornersR)
+ 
  
 # Calibrating left camera
 retL, mtxL, distL, rvecsL, tvecsL = cv2.calibrateCamera(obj_pts,img_ptsL,imgL_gray.shape[::-1],None,None)
